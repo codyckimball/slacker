dd63be270c57d377ef6ebb109f2bc39f98530903
codyckimball <codyckimball@gmail.com>
2017-10-30 22:05:18 +0000

    Workfront PTO

--- app/cody.js
+++ app/cody.js
@@ -1,3 +1,4 @@
+var request = require("request");
 const token = require('../slackTokens/cody.js').token
 
 function getStatus(){
@@ -11,24 +12,32 @@ function currentProcess(events){
   var response = {};
   var now = new Date();
   for (var i = 0; i < events.length; i++){
+    // WORKFRONT
+    if(events[i]["event"].indexOf('OOO') !== -1){
+      workfrontPTO(new Date(events[i]["start"]), new Date(events[i]["end"]));
+    }
+
+    // SLACK
     if(events[i]["start"].indexOf('T') > -1 && events[i]["end"].indexOf('T') > -1){
 	    var now = new Date();
             var startDate = new Date(Date.parse(events[i]["start"],"yyyy-MM-DD'T'HH:mm:ss'-06:00'"));
             var endDate = new Date(Date.parse(events[i]["end"],"yyyy-MM-DD'T'HH:mm:ss'-06:00'"))
+            
             if(startDate < now && now < endDate){
               response = {"token":token, 'status_text':events[i]["event"], "status_emoji":":meeting:"}
-		if(response['status_text'].indexOf('OOO') !== -1){
+		          
+              if(response['status_text'].indexOf('OOO') !== -1){
                 	response['status_emoji'] = ":palm_tree:";
             	} else if (response['status_text'].indexOf('Frontrunner') !== -1){
-			response['status_emoji'] = ":train:";
-		} else {
-			response['status_text'] = 'In Meeting: ' + response['status_text'];
-		}
+          			response['status_emoji'] = ":train:";
+          		} else {
+          			response['status_text'] = 'In Meeting: ' + response['status_text'];
+          		}
 
             }
      } else if(events[i]["event"].indexOf('OOO') !== -1 && new Date(events[i]["start"]).getDate() <= now.getDate() && new Date(events[i]["end"]).getDate() >= now.getDate()){
-     		response = {"token":token, 'status_text':events[i]["event"], "status_emoji":":palm_tree:"}
-		break;
+        response = {"token":token, 'status_text':events[i]["event"], "status_emoji":":palm_tree:"}
+		    break;
      }
   }
   if (response['status_text'] == undefined){
@@ -37,5 +46,24 @@ function currentProcess(events){
   return response;
 }
 
+function workfrontPTO(start,end){
+  console.log(start.toISOString());
+  console.log(end.toISOString());
+  var options = { method: 'GET',
+  url: 'https://tylerreid.attask-ondemand.com/attask/api/v7.0/USER',
+  qs: 
+   { apiKey: 'rxr5rlplwukcu5s2aawvenj0yer1mrl0',
+     method: 'PUT',
+     updates: `[{"ID":"59af1e260099b28dbe3478922381df9c",reservedTimes:[{"objCode":"RESVT","endDate":"${end.toISOString()}","startDate":"${start.toISOString()}"}]}]` 
+   }
+ }
+ request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+ });
+}
+
 module.exports.status = getStatus
 module.exports.currentProcess = currentProcess
+
